7492d6256f8e74df5cebc7f9f8a0cbc6a6bf8196
OfficialSkillarN <45418985+OfficialSkillarN@users.noreply.github.com>
2019-11-24 20:55:23 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ function cameraStart() {
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraSensor.getContext("2d").drawImage(cameraView, 180, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-    constraints = {video: {facingMode: "user" }, audio: false};
 };
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
