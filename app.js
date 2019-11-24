a44bd241b0478d0065e1d014c9ba0c283bb0ca40
OfficialSkillarN <45418985+OfficialSkillarN@users.noreply.github.com>
2019-11-24 23:50:10 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ function cameraStart() {
 }
 
 //Takes screenshot
-cameraTrigger.onclick = function() {
+function cameraTrigger() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-};
+}
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
